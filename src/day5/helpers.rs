46fee9471153ce3c35243651f7edaafcb1cc8106
Fynn Muermanns <fynn.m@live.de>
2023-12-20 14:33:38 +0100

    day 5 puzzle 1 solved

--- src/day5/helpers.rs
+++ src/day5/helpers.rs
@@ -4,20 +4,20 @@ const NUMBER_DELIMITER: &str = " ";
 const END_OF_BLOCK_DELIMITER: &str = "\n";
 
 // get all seeds for which we need to find the lowest location number
-pub fn get_seeds(lines: &Vec<&str>) -> Vec<u32> {
+pub fn get_seeds(lines: &Vec<&str>) -> Vec<u64> {
     const SEED_ID: &str = "seeds: ";
 
     assert!(find_line_idx(SEED_ID, &lines).is_some());
     let seed_line_idx: usize = find_line_idx(SEED_ID, &lines).unwrap();
     let seed_line: &str = lines[seed_line_idx];
 
-    let mut seeds: Vec<u32> = Vec::new();
+    let mut seeds: Vec<u64> = Vec::new();
 
     let seed_line_without_id: &str = seed_line.strip_prefix(SEED_ID).unwrap();
 
     for mut seed in seed_line_without_id.split(NUMBER_DELIMITER) {
         seed = seed.trim();
-        let seed_num: u32 = seed.parse().unwrap();
+        let seed_num: u64 = seed.parse().unwrap();
 
         seeds.push(seed_num);
     }
@@ -27,53 +27,74 @@ pub fn get_seeds(lines: &Vec<&str>) -> Vec<u32> {
 
 // read the seed-to-soil map
 pub fn get_seed_to_soil_map(lines: &Vec<&str>) ->
-    (Vec<Range<u32>>, Vec<u32>) {
+    (Vec<i64>, Vec<Range<u64>>)  {
     const SEED_TO_SOIL_ID: &str = "seed-to-soil map:";
     return parse_map(SEED_TO_SOIL_ID, lines);
 }
 
 // read the soil-to-fertilizer map
 pub fn get_soil_to_fertilizer_map(lines: &Vec<&str>) ->
-    (Vec<Range<u32>>, Vec<u32>) {
+    (Vec<i64>, Vec<Range<u64>>)  {
     const SOIL_TO_FERTILIZER_ID: &str = "soil-to-fertilizer map:";
     return parse_map(SOIL_TO_FERTILIZER_ID, lines);
 }
 
 // read the fertilizer-to-water map
 pub fn get_fertilizer_to_water_map(lines: &Vec<&str>) ->
-    (Vec<Range<u32>>, Vec<u32>) {
+    (Vec<i64>, Vec<Range<u64>>)  {
     const FERTILIZER_TO_WATER_ID: &str = "fertilizer-to-water map:";
     return parse_map(FERTILIZER_TO_WATER_ID, lines);
 }
 
 // read the water-to-light map
 pub fn get_water_to_light_map(lines: &Vec<&str>) ->
-    (Vec<Range<u32>>, Vec<u32>) {
+    (Vec<i64>, Vec<Range<u64>>)  {
     const WATER_TO_LIGHT_ID: &str = "water-to-light map:";
     return parse_map(WATER_TO_LIGHT_ID, lines);
 }
 
 // read the light-to-temperature map
 pub fn get_light_to_temperature_map(lines: &Vec<&str>) ->
-    (Vec<Range<u32>>, Vec<u32>) {
+    (Vec<i64>, Vec<Range<u64>>)  {
     const LIGHT_TO_TEMPERATURE_ID: &str = "light-to-temperature map:";
     return parse_map(LIGHT_TO_TEMPERATURE_ID, lines);
 }
 
 // read the temperature-to-humidity map
 pub fn get_temperature_to_humidity_map(lines: &Vec<&str>) ->
-    (Vec<Range<u32>>, Vec<u32>) {
+    (Vec<i64>, Vec<Range<u64>>)  {
     const TEMPERATURE_TO_HUMIDITY_ID: &str = "temperature-to-humidity map:";
     return parse_map(TEMPERATURE_TO_HUMIDITY_ID, lines);
 }
 
 // read the humidity-to-location map
 pub fn get_humidity_to_location_map(lines: &Vec<&str>) ->
-    (Vec<Range<u32>>, Vec<u32>) {
+    (Vec<i64>, Vec<Range<u64>>)  {
     const HUMIDITY_TO_LOCATION_ID: &str = "humidity-to-location map:";
     return parse_map(HUMIDITY_TO_LOCATION_ID, lines);
 }
 
+pub fn map_number(num: u64, maps: &(Vec<i64>, Vec<Range<u64>>)) -> u64 {
+    match __map_number(num, maps) {
+        Some(mapped_num) => mapped_num,
+        None => num,
+    }
+}
+
+fn __map_number(num: u64, maps: &(Vec<i64>, Vec<Range<u64>>)) -> Option<u64> {
+    for (i, map) in maps.1.iter().enumerate() {
+        if map.contains(&num) {
+            let offset: i64 = maps.0[i];
+
+            assert!(num as i64 + offset > 0);
+            let mapped_number: u64 = (num as i64 + offset) as u64;
+
+            return Some(mapped_number);
+        } 
+    }
+
+    return None;
+}
 
 // try to find a line with the provided line_id/prefixc in a
 // vector of lines and return its index if it could be found
@@ -88,35 +109,40 @@ fn find_line_idx(line_id: &str, lines: &Vec<&str>) -> Option<usize> {
 }
 
 fn parse_map(line_id: &str, lines: &Vec<&str>) ->
-    (Vec<Range<u32>>, Vec<u32>) {
+    (Vec<i64>, Vec<Range<u64>>)  {
     assert!(find_line_idx(line_id, &lines).is_some());
+
     let map_start_idx: usize =
         find_line_idx(line_id, &lines).unwrap() + 1;
     
     let mut i: usize = map_start_idx;
     let mut line: &str = lines[i];
 
-    let mut destination_ranges: Vec<Range<u32>> = Vec::new();
-    let mut source_offsets: Vec<u32> = Vec::new();
+    let mut source_ranges: Vec<Range<u64>> = Vec::new();
+    let mut destination_offsets: Vec<i64> = Vec::new();
 
-    while line != END_OF_BLOCK_DELIMITER {
-        let nums: Vec<u32> =
+    while !line.is_empty() {
+        let nums: Vec<i64> =
             line.trim().split(NUMBER_DELIMITER).map(
                 |l| l.trim().parse().unwrap()).collect();
 
         assert!(nums.len() == 3);
-        
-        let range_len: u32 = nums[2];
-        let destination_range_start: u32 = nums[0];
-        let source_range_start: u32 = nums[1];
 
-        source_offsets.push(source_range_start);
-        destination_ranges.push(
-            destination_range_start..destination_range_start+range_len);
+        let destination_range_start: i64 = nums[0];
+        let source_range_start: i64 = nums[1];
+        let range_len: i64 = nums[2];
+
+        let destination_offset: i64 =
+            destination_range_start - source_range_start;
+
+        destination_offsets.push(destination_offset);
+
+        source_ranges.push(
+            source_range_start as u64..(source_range_start+range_len) as u64);
 
         i += 1;
         line = lines[i];
     }
 
-    return (destination_ranges, source_offsets);
+    return (destination_offsets, source_ranges);
 }
--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -2,47 +2,122 @@ use std::ops::Range;
 
 mod helpers;
 
-// TODO: just save offset instead of destination range
-// iterate through all ranges of map and check if key is contained
-// in any of the ranges; if not, simply pass same key to next map;
-// if so, add offset to key and pass to next map
-
 pub fn puzzle1(file_content: String) -> String {
-    let lines: Vec<&str> =
-        file_content.split("\n").map(|l| l.trim()).filter(
-            |l| !l.is_empty()).collect();
+    let lines: Vec<&str> = file_content.split("\n").collect();
 
-    for mut line in file_content.split("\n") {
-        line = line.trim();   // strip whitespace
-        if line.is_empty() {  // skip empty lines
-            continue;
+    let seeds: Vec<u64> = helpers::get_seeds(&lines);
+
+    let seed_to_soil_map: (Vec<i64>, Vec<Range<u64>>) =
+        helpers::get_seed_to_soil_map(&lines);
+    
+    let soil_to_fertilizer_map: (Vec<i64>, Vec<Range<u64>>) =
+        helpers::get_soil_to_fertilizer_map(&lines);
+
+    let fertilizer_to_water_map: (Vec<i64>, Vec<Range<u64>>) =
+        helpers::get_fertilizer_to_water_map(&lines);
+    
+    let water_to_light_map: (Vec<i64>, Vec<Range<u64>>) =
+        helpers::get_water_to_light_map(&lines);
+    
+    let light_to_temperature_map: (Vec<i64>, Vec<Range<u64>>) =
+        helpers::get_light_to_temperature_map(&lines);
+
+    let temperature_to_humidity_map: (Vec<i64>, Vec<Range<u64>>) =
+        helpers::get_temperature_to_humidity_map(&lines);
+
+    let humidity_to_location_map: (Vec<i64>, Vec<Range<u64>>) =
+        helpers::get_humidity_to_location_map(&lines);
+
+    let mut lowest_location: u64 = u64::MAX;
+
+    for seed in seeds {
+        let soil_number: u64 = helpers::map_number(
+            seed, &seed_to_soil_map);
+
+        let fertilizer_number: u64 = helpers::map_number(
+            soil_number, &soil_to_fertilizer_map);
+
+        let water_number: u64 = helpers::map_number(
+            fertilizer_number, &fertilizer_to_water_map);
+    
+        let light_number: u64 = helpers::map_number(
+            water_number, &water_to_light_map);
+
+        let temperature_number: u64 = helpers::map_number(
+            light_number, &light_to_temperature_map);
+        
+        let humidity_number: u64 = helpers::map_number(
+            temperature_number, &temperature_to_humidity_map);
+
+        let location_number: u64 = helpers::map_number(
+            humidity_number, &humidity_to_location_map);
+        
+        //println!("{seed}, {soil_number}, {fertilizer_number}, {water_number}, {light_number}, {temperature_number}, {humidity_number}, {location_number}");
+
+        if location_number < lowest_location {
+            lowest_location = location_number;
         }
     }
 
-    let seeds: Vec<u32> = helpers::get_seeds(&lines);
+    return lowest_location.to_string();
+}
 
-    let seed_to_soil_map: (Vec<Range<u32>>, Vec<u32>) =
+pub fn puzzle2(file_content: String) -> String {
+    let lines: Vec<&str> = file_content.split("\n").collect();
+
+    let seeds: Vec<u64> = helpers::get_seeds(&lines);
+
+    let seed_to_soil_map: (Vec<i64>, Vec<Range<u64>>) =
         helpers::get_seed_to_soil_map(&lines);
     
-    let soil_to_fertilizer_map: (Vec<Range<u32>>, Vec<u32>) =
+    let soil_to_fertilizer_map: (Vec<i64>, Vec<Range<u64>>) =
         helpers::get_soil_to_fertilizer_map(&lines);
 
-    let fertilizer_to_water_map: (Vec<Range<u32>>, Vec<u32>) =
+    let fertilizer_to_water_map: (Vec<i64>, Vec<Range<u64>>) =
         helpers::get_fertilizer_to_water_map(&lines);
     
-    let water_to_light_map: (Vec<Range<u32>>, Vec<u32>) =
+    let water_to_light_map: (Vec<i64>, Vec<Range<u64>>) =
         helpers::get_water_to_light_map(&lines);
     
-    let light_to_temperature_map: (Vec<Range<u32>>, Vec<u32>) =
+    let light_to_temperature_map: (Vec<i64>, Vec<Range<u64>>) =
         helpers::get_light_to_temperature_map(&lines);
 
-    let temperature_to_humidity_map: (Vec<Range<u32>>, Vec<u32>) =
+    let temperature_to_humidity_map: (Vec<i64>, Vec<Range<u64>>) =
         helpers::get_temperature_to_humidity_map(&lines);
 
-    let humidity_to_location_map: (Vec<Range<u32>>, Vec<u32>) =
+    let humidity_to_location_map: (Vec<i64>, Vec<Range<u64>>) =
         helpers::get_humidity_to_location_map(&lines);
 
-    let lowest_location: u32 = 0;
+    let mut lowest_location: u64 = u64::MAX;
+
+    for seed in seeds {
+        let soil_number: u64 = helpers::map_number(
+            seed, &seed_to_soil_map);
+
+        let fertilizer_number: u64 = helpers::map_number(
+            soil_number, &soil_to_fertilizer_map);
+
+        let water_number: u64 = helpers::map_number(
+            fertilizer_number, &fertilizer_to_water_map);
+    
+        let light_number: u64 = helpers::map_number(
+            water_number, &water_to_light_map);
+
+        let temperature_number: u64 = helpers::map_number(
+            light_number, &light_to_temperature_map);
+        
+        let humidity_number: u64 = helpers::map_number(
+            temperature_number, &temperature_to_humidity_map);
+
+        let location_number: u64 = helpers::map_number(
+            humidity_number, &humidity_to_location_map);
+        
+        //println!("{seed}, {soil_number}, {fertilizer_number}, {water_number}, {light_number}, {temperature_number}, {humidity_number}, {location_number}");
+
+        if location_number < lowest_location {
+            lowest_location = location_number;
+        }
+    }
 
     return lowest_location.to_string();
 }
