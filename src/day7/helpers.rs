556be75a01528a4adef530f21b020a7be96d0593
Fynn Muermanns <fynn.m@live.de>
2023-12-25 18:18:13 +0100

    some progess on day 7 puzzle 1 (still haven't correctly implemented determination of stronger hand)

--- src/day7/helpers.rs
+++ src/day7/helpers.rs
@@ -1,8 +1,7 @@
 use std::collections::HashMap;
 use std::cmp::Ordering;
-use std::ops::BitAnd;
 
-#[derive(PartialEq, PartialOrd)]
+#[derive(Debug, PartialEq, Eq, PartialOrd)]
 enum Rank {
     FiveOfKind  = 7,
     FourofKind  = 6,
@@ -13,11 +12,94 @@ enum Rank {
     HighCard    = 1,
 }
 
+impl Rank {
+    // check if all cards are equal (since a consists of exaclty 5 cards)
+    fn is_five_of_kind(hand: &str) -> bool {
+        let mut hand = hand.bytes();
+        let first_chr: u8 = hand.next().unwrap();
+        return hand.all(|c| c == first_chr);
+    }
+
+    // if highest card count is 4, remaining card is irrelevant
+    fn is_four_of_kind(hand: &str) -> bool {
+        let (_counts, high_count): (HashMap<char, u8>, u8) = count_cards(hand);
+
+        return high_count == 4;
+    }
+
+    fn is_three_of_kind(hand: &str) -> bool {
+        let (counts, _high_count): (HashMap<char, u8>, u8) = count_cards(hand);
+
+        let mut pair_count: u8 = 0;
+        let mut triplet_count: u8 = 0;
+
+        for (_card, count) in counts {
+            if count == 2 {
+                pair_count += 1;
+            } else if count == 3 {
+                triplet_count += 1;
+            }
+        }
+
+        return triplet_count == 1 && pair_count == 0;
+    }
+
+    fn is_full_house(hand: &str) -> bool {
+        let (counts, _high_count): (HashMap<char, u8>, u8) = count_cards(hand);
+
+        let mut pair_count: u8 = 0;
+        let mut triplet_count: u8 = 0;
+
+        for (_card, count) in counts {
+            if count == 2 {
+                pair_count += 1;
+            } else if count == 3 {
+                triplet_count += 1;
+            }
+        }
+
+        return triplet_count == 1 && pair_count == 1;
+    }
+
+    fn is_two_pair(hand: &str) -> bool {
+        let (counts, _high_count): (HashMap<char, u8>, u8) = count_cards(hand);
+
+        let mut pair_count: u8 = 0;
+
+        for (_card, count) in counts {
+            if count == 2 {
+                pair_count += 1;
+            }
+        }
+
+        return pair_count == 2;
+    }
+
+    // if highest card count is 2, all other cards must be different
+    fn is_single_pair(hand: &str) -> bool {
+        let (counts, _high_count): (HashMap<char, u8>, u8) = count_cards(hand);
+
+        let mut pair_count: u8 = 0;
+        let mut triplet_count: u8 = 0;
+
+        for (_card, count) in counts {
+            if count == 2 {
+                pair_count += 1;
+            } else if count == 3 {
+                triplet_count += 1;
+            }
+        }
+
+        return pair_count == 1 && triplet_count == 0;
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, PartialOrd)]
 pub struct Hand<'a> {
     hand: &'a str,
     bid: u32,
-
+    rank: Rank,
+    hand_strength: u32,
 }
 
 impl Ord for Hand<'_> {
@@ -35,30 +117,23 @@ impl Ord for Hand<'_> {
 }
 
 impl Hand<'_> {
-    pub fn beats(&self, other_hand: &Hand) -> bool {
-        let own_rank: Rank = self.get_rank();
-        let other_rank: Rank = other_hand.get_rank();
-
-        if own_rank == other_rank {
-            return self.is_hand_stronger(other_hand);
+    pub fn beats(&self, other: &Hand) -> bool {
+        if self.rank == other.rank {
+            return self.is_hand_stronger(other);
         } else {
-            return own_rank > other_rank;
+            return self.rank > other.rank;
         }
-    }
-
-    fn get_rank(&self) -> Rank {
-        // TODO: parse hand string and return the rank of the hand
     }
 
     // check if hand is stronger than the other hand if they
     // both share the same rank (i.e. both hands contain 2 pairs)
-    fn is_hand_stronger(&self, other_hand: &Hand) -> bool {
-        // TODO: compare each card for both hands and return if
-        //       the self card has the stronger hand
+    fn is_hand_stronger(&self, other: &Hand) -> bool {
+        return self.hand_strength > other.hand_strength;
     }
 }
 
-pub fn parse_hand(line: &str) -> Hand {
+pub fn parse_hand<'a> (
+    line: &'a str, rank_map: &HashMap<char, u32>) -> Hand<'a> {
     const DELIMITER: &str = " ";
 
     assert!(line.find(DELIMITER).is_some());
@@ -66,8 +141,10 @@ pub fn parse_hand(line: &str) -> Hand {
 
     let hand: &str = &line[0..delimiter_idx];
     let bid: u32 = (&line[delimiter_idx+1..]).parse().unwrap();
+    let rank: Rank = get_rank(hand);
+    let hand_strength: u32 = calc_hand_strength(hand, rank_map);
 
-    return Hand{hand: hand, bid: bid};
+    return Hand{hand: hand, bid: bid, rank: rank, hand_strength: hand_strength};
 }
 
 pub fn calc_total_winnings(ranked_hands: &Vec<Hand>) -> u32 {
@@ -81,4 +158,58 @@ pub fn calc_total_winnings(ranked_hands: &Vec<Hand>) -> u32 {
     }
 
     return total_winnings;
+}
+
+fn get_rank(hand: &str) -> Rank {
+    let rank: Rank = if Rank::is_five_of_kind(hand) {
+        Rank::FiveOfKind
+    } else if Rank::is_four_of_kind(hand) {
+        Rank::FourofKind
+    } else if Rank::is_full_house(hand) {
+        Rank::FullHouse
+    } else if Rank::is_three_of_kind(hand) {
+        Rank::ThreeOfKind
+    } else if Rank::is_two_pair(hand) {
+        Rank::TwoPair
+    } else if Rank::is_single_pair(hand) {
+        Rank::SinglePair
+    } else {
+        Rank::HighCard
+    };
+
+    return rank;
+}
+
+fn count_cards(hand: &str) -> (HashMap<char, u8>, u8) {
+    let mut card_counts: HashMap<char, u8> = HashMap::new();
+
+    let mut highest_card_count: u8 = 0;
+
+    for card in hand.chars() {
+        let entry: Option<&mut u8> = card_counts.get_mut(&card);
+        if entry.is_some() {
+            let count: &mut u8 = entry.unwrap();
+            *count += 1;
+
+            if *count > highest_card_count {
+                highest_card_count = *count;
+            }
+        } else {
+            card_counts.insert(card, 1);
+        }
+    }
+
+    return (card_counts, highest_card_count);
+}
+
+fn calc_hand_strength(hand: &str, rank_map: &HashMap<char, u32>) -> u32 {
+    let total_cards: usize = hand.len();
+    let mut hand_strength: u32 = 0;
+
+    for (i, card) in hand.chars().enumerate() {
+        let weight_of_card: u32 = (total_cards - i) as u32;
+        hand_strength += rank_map[&card] * weight_of_card;
+    }
+
+    return hand_strength;
 }
--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 use helpers::Hand;
 
 pub fn puzzle1(file_content: String) -> String {
-    let rank_map: HashMap<char, u8> = HashMap::from([
+    let rank_map: HashMap<char, u32> = HashMap::from([
         ('2', 1),
         ('3', 2),
         ('4', 3),
@@ -24,10 +24,11 @@ pub fn puzzle1(file_content: String) -> String {
 
     for mut line in file_content.split("\n") {
         line = line.trim();
-        hands.push(helpers::parse_hand(line));
+        hands.push(helpers::parse_hand(line, &rank_map));
     }
 
     hands.sort();
+    println!("{:?}", hands);
 
     let total_winnings: u32 = helpers::calc_total_winnings(&hands);
 
