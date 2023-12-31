0a072cf1b2ea66a408e4b7a96c8ef38f5b43e089
Fynn Muermanns <fynn.m@live.de>
2023-12-31 23:52:18 +0100

    try to parse pipe loop (not working yet)

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -1,11 +1,11 @@
 mod helpers;
-use helpers::GridPosition;
+use helpers::LoopPipe;
 
 pub fn puzzle1(file_content: String) -> String {
     let pipe_maze: Vec<&str> = helpers::parse_pipe_maze(&file_content);
 
     assert!(helpers::find_start_pipe(&pipe_maze).is_some());
-    let start_position: GridPosition =
+    let start_position: LoopPipe =
       helpers::find_start_pipe(&pipe_maze).unwrap();
     
     let furthest_distance: u32 = helpers::find_furthest_distance_from_start(
